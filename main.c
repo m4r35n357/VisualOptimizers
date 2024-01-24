9e824b190a5187d26e082e75817a45013c94962f
Ian Smith <m4r35n357@gmail.com>
2024-01-24 14:48:13 +0000

    text-mode now dual-simplex too

--- main.c
+++ main.c
@@ -15,31 +15,48 @@ int main(int argc, char **argv) {
     for (int i = 0; i < n; i++) {
         start->x[i] = strtod(argv[i + 6], NULL);
     }
-    point *solution = get_point(n);
+    point *solution1 = get_point(n);
+    point *solution2 = get_point(n);
 
     // model parameters
     model *m = model_init();
 
-    // get a simplex
-    simplex *s = get_simplex(n, o.size, start);
-    for (int i = 0; i < s->n + 1; i++) {  // initial cost at simplex vertices
-        cost(s->n, s->p + i, m);
-        s->evaluations++;
+    // default simplex . . .
+    simplex *s1 = get_simplex(n, o.size, start);
+    for (int i = 0; i < s1->n + 1; i++) {  // initial cost at simplex vertices
+        cost(s1->n, s1->p + i, m);
+        s1->evaluations++;
     }
-    sort(s);
-
+    sort(s1);
     // begin optimization
-    nelder_mead(s, solution, m, &o);
-
+    nelder_mead(s1, solution1, m, &o);
     // print starting point
     fprintf(stderr, "%s     Initial ", GRY);
     cost(n, start, m);
     print_point(n, start, o.places, o.fmt);
     // print solution
     fprintf(stderr, "    %sSolution ", GRY);
-    print_point(n, solution, o.places, o.fmt);
+    print_point(n, solution1, o.places, o.fmt);
     // print stats
-    fprintf(stderr, "%s  Iterations/Evaluations%s %d/%d\n", GRY, NRM, s->iterations, s->evaluations);
+    fprintf(stderr, "%s  Iterations/Evaluations%s %d/%d\n", GRY, NRM, s1->iterations, s1->evaluations);
+
+    // . . . and its "dual"
+    simplex *s2 = get_simplex(n, o.size, start);
+    for (int i = 0; i < s2->n + 1; i++) {  // form "dual" by projecting vertices through the centre
+        project(s2->p + i, s2, m, start, 1.0L, s2->p + i, start);
+    }
+    sort(s2);
+    // begin optimization
+    nelder_mead(s2, solution2, m, &o);
+    // print starting point
+    fprintf(stderr, "%s     Initial ", GRY);
+    cost(n, start, m);
+    print_point(n, start, o.places, o.fmt);
+    // print solution
+    fprintf(stderr, "    %sSolution ", GRY);
+    print_point(n, solution2, o.places, o.fmt);
+    // print stats
+    fprintf(stderr, "%s  Iterations/Evaluations%s %d/%d\n", GRY, NRM, s2->iterations, s2->evaluations);
 
     return 0;
 }
