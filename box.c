8422482f44f11c9e0a6386d6933ffc7f3369d178
Ian Smith <m4r35n357@gmail.com>
2024-01-29 12:23:20 +0000

    verbose explanation whitespace

--- box.c
+++ box.c
@@ -6,8 +6,8 @@ struct Model { real area, min_edge; };
 
 model *model_init () {
     model *m = malloc(sizeof (model));
-    m->area = 48.0L;      // equality constraint
-    m->min_edge = 0.01L;  // inequality constraint
+    m->area = 48.0L;       // equality constraint
+    m->min_edge = 0.001L;  // inequality constraint
     return m;
 }
 
@@ -16,7 +16,9 @@ minima *get_known_minima () {
 }
 
 void cost (int n, point *p, const model *m) { (void)n;
-	p->x[0] = p->x[0] >= m->min_edge ? p->x[0] : m->min_edge;
-	p->x[1] = p->x[1] >= m->min_edge ? p->x[1] : m->min_edge;
-	p->f = m->area / (p->x[0] * p->x[1] * (m->area - 2.0L * p->x[0] * p->x[1]) / (2.0L * (p->x[0] + p->x[1])));
+    p->x[0] = p->x[0] >= m->min_edge ? p->x[0] : m->min_edge;                      // IC
+    p->x[1] = p->x[1] >= m->min_edge ? p->x[1] : m->min_edge;                      // IC
+    real Z = (m->area - 2.0L * p->x[0] * p->x[1]) / (2.0L * (p->x[0] + p->x[1]));  // EC eliminates a variable
+    Z = Z >= m->min_edge ? Z : m->min_edge;                                        // IC
+    p->f = m->area / (p->x[0] * p->x[1] * Z);
 }
