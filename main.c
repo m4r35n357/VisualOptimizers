7a9b79f763764f9dff4b2cd99fc6a90ff3a119c4
Ian Smith <m4r35n357@gmail.com>
2024-04-07 12:02:33 +0100

    only use "dual" simplexes in OpenGL

--- main.c
+++ main.c
@@ -26,39 +26,33 @@ int main (int argc, char **argv) {
     }
     cost(o.n, start, m);
 
-    simplex *s1 = nm_simplex(o.n, o.size, start, o.adaptive);
-    simplex *s2 = nm_simplex(o.n, o.size, start, o.adaptive);
+    simplex *s = nm_simplex(o.n, o.size, start, o.adaptive);
     fprintf(stderr, o.fmt ? "      %sDiameter %s% .*Le\n" : "      %sDiameter%s    % .*Lf\n",
-            GRY, NRM, o.places, distance(o.n, s1->p, s1->p + o.n));
+            GRY, NRM, o.places, distance(o.n, s->p, s->p + o.n));
 
     int runs = 1, iterations = 0, evaluations = 1;
     point *boat = get_point(o.n);
     copy_point(o.n, start, boat);
     do {
         for (int i = 0; i < o.n + 1; i++) {
-            cost(o.n, s1->p + i, m);
-            s1->evaluations++;
-            project(s2->p + i, s2, m, 1.0L, s2->p + i, start);
+            cost(o.n, s->p + i, m);
+            s->evaluations++;
         }
-        sort(s1);
-        sort(s2);
-        nelder_mead(s1, m, &o);
-        nelder_mead(s2, m, &o);
-        iterations += s1->iterations + s2->iterations;
-        evaluations += s1->evaluations + s2->evaluations;
+        sort(s);
+        nelder_mead(s, m, &o);
+        iterations += s->iterations;
+        evaluations += s->evaluations;
 
         fprintf(stderr, "\r                         \r%5d %8d %8d ", runs, iterations, evaluations);
-        point *best = s1->p[0].f <= s2->p[0].f ? s1->p : s2->p;
-        if (best->f < boat->f) {
-            copy_point(o.n, best, boat);
+        if (s->p[0].f < boat->f) {
+            copy_point(o.n, s->p, boat);
             fprintf(stderr, "\r%5d %8d %8d ", runs, iterations, evaluations);
             print_result(o.n, boat, o.places, o.fmt);
         }
 
         if (o.init_mode) {
             set_random_coordinates(start, o.n, o.lower, o.upper);
-            regular_simplex(s1, o.size, start);
-            regular_simplex(s2, o.size, start);
+            regular_simplex(s, o.size, start);
         }
     } while (runs++ < o.init_mode);
 
