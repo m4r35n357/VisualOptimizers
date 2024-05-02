77058bc1060554c67b9dec5939e847d206ad88a5
Ian Smith <m4r35n357@gmail.com>
2024-05-02 20:52:15 +0100

    simplify random

--- main-random.c
+++ main-random.c
@@ -7,46 +7,39 @@ int main (int argc, char **argv) {
     PRINT_ARGS(argc, argv);
     CHECK(argc == 7);
 
-    // optimizer settings
     randomize();
-    optimset o = {
-        .places = (int)strtol(argv[1], NULL, BASE),
-        .fmt = (int)strtol(argv[2], NULL, BASE),
-        .n = (int)strtol(argv[3], NULL, BASE),
-        .max_evaluations = (int)strtol(argv[4], NULL, BASE),
-        .step_mode = false
-    };
-    CHECK(o.places >= 1 && o.places <= 36);
-    CHECK(o.fmt == 0 || o.fmt == 1);
-    CHECK(o.n >= 1);
-    CHECK(o.max_evaluations >= 0 && o.max_evaluations <= 100000000);
+
+    int places = (int)strtol(argv[1], NULL, BASE); CHECK(places >= 1 && places <= 36);
+    int fmt = (int)strtol(argv[2], NULL, BASE); CHECK(fmt == 0 || fmt == 1);
+    int n = (int)strtol(argv[3], NULL, BASE); CHECK(n >= 1);
+    int max_evaluations = (int)strtol(argv[4], NULL, BASE); CHECK(max_evaluations >= 0 && max_evaluations <= 100000000);
 
     // model parameters
     model *m = model_init();
 
-    point *centre = get_point(o.n);
-    o.lower = strtold(argv[5], NULL);
-    o.upper = strtold(argv[6], NULL);  CHECK(o.upper > o.lower);
-    set_random_coordinates(centre, o.n, o.lower, o.upper);
-    cost(o.n, centre, m);
+    point *centre = get_point(n);
+    real lower = strtold(argv[5], NULL);
+    real upper = strtold(argv[6], NULL);  CHECK(upper > lower);
+    set_random_coordinates(centre, n, lower, upper);
+    cost(n, centre, m);
 
-    point *boat = get_point(o.n);
+    point *boat = get_point(n);
     boat->f = INFINITY;
     int runs = 0, evaluations = 1;
     do {
         runs++;
-        cost(o.n, centre, m);
+        cost(n, centre, m);
         evaluations++;
 
         if (centre->f < boat->f) {
-            copy_point(o.n, centre, boat);
+            copy_point(n, centre, boat);
         }
 
-        set_random_coordinates(centre, o.n, o.lower, o.upper);
-    } while (evaluations < o.max_evaluations);
+        set_random_coordinates(centre, n, lower, upper);
+    } while (evaluations < max_evaluations);
 
     fprintf(stderr, "%8d %8d  ", runs, evaluations);
-    print_result(o.n, boat, o.places, o.fmt);
+    print_result(n, boat, places, fmt);
 
     return 0;
 }
