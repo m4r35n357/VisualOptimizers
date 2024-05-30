ac38ceb66af87605d2411a07a2001a14468ce3ef
Ian Smith <m4r35n357@gmail.com>
2024-05-30 15:02:44 +0100

    simplification to 6D

--- xor.c
+++ xor.c
@@ -19,9 +19,8 @@ minima *get_known_minima (int n) { (void)n;
     return NULL;
 }
 
-static real nn (real i1, real w1, real i2, real w2, real b) {
-    //return tanhl(i1 * w1 + i2 * w2 - b);
-    real tmp = i1 * w1 + i2 * w2 - b;
+static real nn (real i1, real i2, real w, real b) {
+    real tmp = w * (i1 + i2) - b;
     return tmp > 0.0L ? 1.0L : 0.0L;
 }
 
@@ -34,18 +33,13 @@ void cost (int n, point *p, const model *m)  { (void)n; (void)m;
     }
     p->f = 0.0L;
 
-    p->f += SQR(0.0L - nn(nn(0.0L, p->x[0], 0.0L, p->x[1], p->x[2]), p->x[6], nn(0.0L, p->x[3], 0.0L, p->x[4], p->x[5]), p->x[7], p->x[8]));
-    p->f += SQR(1.0L - nn(nn(0.0L, p->x[0], 1.0L, p->x[1], p->x[2]), p->x[6], nn(0.0L, p->x[3], 1.0L, p->x[4], p->x[5]), p->x[7], p->x[8]));
-    p->f += SQR(1.0L - nn(nn(1.0L, p->x[0], 0.0L, p->x[1], p->x[2]), p->x[6], nn(1.0L, p->x[3], 0.0L, p->x[4], p->x[5]), p->x[7], p->x[8]));
-    p->f += SQR(0.0L - nn(nn(1.0L, p->x[0], 1.0L, p->x[1], p->x[2]), p->x[6], nn(1.0L, p->x[3], 1.0L, p->x[4], p->x[5]), p->x[7], p->x[8]));
+    p->f += SQR(0.0L - nn(nn(0.0L, 0.0L, p->x[0], p->x[1]), nn(0.0L, 0.0L, p->x[2], p->x[3]), p->x[4], p->x[5]));
+    p->f += SQR(1.0L - nn(nn(0.0L, 1.0L, p->x[0], p->x[1]), nn(0.0L, 1.0L, p->x[2], p->x[3]), p->x[4], p->x[5]));
+    p->f += SQR(1.0L - nn(nn(1.0L, 0.0L, p->x[0], p->x[1]), nn(1.0L, 0.0L, p->x[2], p->x[3]), p->x[4], p->x[5]));
+    p->f += SQR(0.0L - nn(nn(1.0L, 1.0L, p->x[0], p->x[1]), nn(1.0L, 1.0L, p->x[2], p->x[3]), p->x[4], p->x[5]));
 
-    p->f += SQR(0.0L - nn(nn(0.1L, p->x[0], 0.1L, p->x[1], p->x[2]), p->x[6], nn(0.1L, p->x[3], 0.1L, p->x[4], p->x[5]), p->x[7], p->x[8]));
-    p->f += SQR(1.0L - nn(nn(0.1L, p->x[0], 0.9L, p->x[1], p->x[2]), p->x[6], nn(0.1L, p->x[3], 0.9L, p->x[4], p->x[5]), p->x[7], p->x[8]));
-    p->f += SQR(1.0L - nn(nn(0.9L, p->x[0], 0.1L, p->x[1], p->x[2]), p->x[6], nn(0.9L, p->x[3], 0.1L, p->x[4], p->x[5]), p->x[7], p->x[8]));
-    p->f += SQR(0.0L - nn(nn(0.9L, p->x[0], 0.9L, p->x[1], p->x[2]), p->x[6], nn(0.9L, p->x[3], 0.9L, p->x[4], p->x[5]), p->x[7], p->x[8]));
-
-    p->f += SQR(0.0L - nn(nn(0.2L, p->x[0], 0.2L, p->x[1], p->x[2]), p->x[6], nn(0.2L, p->x[3], 0.2L, p->x[4], p->x[5]), p->x[7], p->x[8]));
-    p->f += SQR(1.0L - nn(nn(0.2L, p->x[0], 0.8L, p->x[1], p->x[2]), p->x[6], nn(0.2L, p->x[3], 0.8L, p->x[4], p->x[5]), p->x[7], p->x[8]));
-    p->f += SQR(1.0L - nn(nn(0.8L, p->x[0], 0.2L, p->x[1], p->x[2]), p->x[6], nn(0.8L, p->x[3], 0.2L, p->x[4], p->x[5]), p->x[7], p->x[8]));
-    p->f += SQR(0.0L - nn(nn(0.8L, p->x[0], 0.8L, p->x[1], p->x[2]), p->x[6], nn(0.8L, p->x[3], 0.8L, p->x[4], p->x[5]), p->x[7], p->x[8]));
+    p->f += SQR(0.0L - nn(nn(0.2L, 0.2L, p->x[0], p->x[1]), nn(0.2L, 0.2L, p->x[2], p->x[3]), p->x[4], p->x[5]));
+    p->f += SQR(1.0L - nn(nn(0.2L, 0.8L, p->x[0], p->x[1]), nn(0.2L, 0.8L, p->x[2], p->x[3]), p->x[4], p->x[5]));
+    p->f += SQR(1.0L - nn(nn(0.8L, 0.2L, p->x[0], p->x[1]), nn(0.8L, 0.2L, p->x[2], p->x[3]), p->x[4], p->x[5]));
+    p->f += SQR(0.0L - nn(nn(0.8L, 0.8L, p->x[0], p->x[1]), nn(0.8L, 0.8L, p->x[2], p->x[3]), p->x[4], p->x[5]));
 }
